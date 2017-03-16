f671d0d60e9ba7c5bd50c2fd009eb73621e34e5e
Gauntly <gauntly@protonmail.com>
2017-03-17 10:40:24 +1300

    Initial Commit

---
+++ daysarray.php
@@ -0,0 +1,16 @@
+<h1>The Days of the week in English are: </h1>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Gauntly
+ * Date: 17-Mar-17
+ * Time: 9:59 AM
+ */
+
+// We simply define the array "days" and in the array argument we put our strings inside.
+$days = array("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday");
+// We next use a foreach loop where we
+foreach( $days as $value ){
+    echo" $value </br >";
+}
+    ?>
