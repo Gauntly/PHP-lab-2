bc5be1c4bd70c302441a82aa248e1994c569d8bd
Gauntly <gauntly@protonmail.com>
2017-03-17 11:55:27 +1300

    Task 3 Currently Broken but going to class now to finish it.

---
+++ factorial.php
@@ -0,0 +1,11 @@
+<title>WebDev Lab 2</title>
+<?php require 'mathfunctions.php';
+/**
+ * Created by PhpStorm.
+ * User: Gauntly
+ * Date: 17-Mar-17
+ * Time: 11:29 AM
+ */
+$number = $_POST["number"];
+echo(factorial($number));
+?>
---
+++ mathfunctions.php
@@ -0,0 +1,21 @@
+<title>WebDev Lab 2</title>
+<h1>Lab 2</h1>
+<h3>Task 3</h3>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Gauntly
+ * Date: 17-Mar-17
+ * Time: 11:20 AM
+ */
+
+function factorial($positiveInt){
+    $result = 1;
+    $factor = $positiveInt;
+    while($factor > 1){
+        $result = $result * $factor;
+        $factor--;
+    }
+    return $result;
+}
+?>
